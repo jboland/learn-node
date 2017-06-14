07459d9c48d92223ab69c12ce0619f18dfe12090
Joseph Boland <jboland@about.com>
2017-06-13 20:25:03 -0400

    Wire up editStores, display all stores

--- app/controllers/storeController.js
+++ app/controllers/storeController.js
@@ -12,8 +12,32 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  console.log('store is saved', store);
-  res.redirect('/');
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully created ${store.name}.  Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+}
+
+exports.getStores = async (req, res) => {
+  // get all stores from DB
+  const stores = await Store.find();
+  console.log(stores);
+  res.render('stores', { title: 'Stores', stores });
+}
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findById(req.params.id);
+  res.render('editStore', {
+    title: 'Edit Store',
+    store
+  });
+}
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
+    new: true, // returns new store
+    runValidators: true
+  }).exec();
+
+  req.flash('success', `Successfully updated ${store.name}. <a href="/stores/${store.slug}">View store</a>.`);
+  res.redirect(`/stores/${store.id}/edit`);
 }
