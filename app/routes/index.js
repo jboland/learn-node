2d426f6c2d63510c7f2913b7bcd50aa9257ff9d6
Joe Boland <josephjboland@gmail.com>
2017-07-05 07:43:43 -0400

    Add user controller, login/register forms, base validation

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers')
 
 // Do work here
@@ -27,5 +28,10 @@ router.get('/store/:slug', catchErrors(storeController.showStore));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+router.post('/register', userController.validateRegister);
+router.post('/login', userController.loginForm);
 
 module.exports = router;
