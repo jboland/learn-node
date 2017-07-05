51940eba380f5b2c7c811fabea33307084417a31
Joseph Boland <jboland@about.com>
2017-07-05 18:43:04 -0400

    Add accounts page/route, functionality to update user account

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -34,8 +34,30 @@ exports.validateRegister = (req, res, next) => {
 
 exports.register = async (req, res, next) => {
   const user = new User({ email: req.body.email, name: req.body.name });
-  
   const register = promisify(User.register, User);
   await register(user, req.body.password);
   next();
+}
+
+exports.account = (req, res) => {
+  res.render('account', { title: 'Edit your account' });
+}
+
+exports.updateAccount = async (req, res) => { 
+  const updates = {
+    name: req.body.name,
+    email: req.body.email
+  };
+
+  const user = await User.findOneAndUpdate(
+    { _id: req.user._id },
+    { $set: updates },
+    { 
+      new: true,
+      runValidators: true,
+      context: 'query'
+    }
+  );
+  req.flash('success', 'Updated profile!');
+  res.redirect('/account');
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -45,4 +45,15 @@ router.post('/register',
 );
 
 router.get('/logout', authController.logout);
+
+router.get('/account',
+  authController.isLoggedIn,
+  userController.account
+);
+
+router.post('/account', 
+  authController.isLoggedIn,
+  catchErrors(userController.updateAccount)
+);
+
 module.exports = router;
